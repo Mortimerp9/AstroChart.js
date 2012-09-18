f4a2de5673d4a6bc46247de3098665a52519aafe
Pierre Andrews <pierre.andrews@gmail.com>
2012-09-18 17:56:53 -0300

    adde other debug cases

---
+++ debug2.js
@@ -0,0 +1,32 @@
+var hc = [0,
+		  266.33599853515625,
+		  290.22100830078125,
+		  315.11700439453125,
+		  343.8710021972656,
+		  17.511699676513672,
+		  53.14580154418945,
+		  86.33609771728516,
+		  110.22100067138672,
+		  135.11700439453125,
+		  163.87100219726562,
+		  197.51199340820312,
+		  233.14599609375];
+
+var longitude = [];
+longitude[5]={planet:5,angle:338.1929931640625,house:12} ;
+longitude[1]={planet:1,angle:304.29901123046875,house:11} ;
+longitude[3]={planet:3,angle:206.1269989013672,house:7} ;
+longitude[0]={planet:0,angle:209.7030029296875,house:7} ;
+longitude[6]={planet:6,angle:108.84700012207031,house:4} ;
+longitude[2]={planet:2,angle:214.33700561523438,house:8} ;
+longitude[4]={planet:4,angle:206.781005859375,house:7} ;
+longitude[7]={planet:7,angle:208.23899841308594,house:7} ;
+longitude[8]={planet:8,angle:247.9409942626953,house:9} ;
+longitude[9]={planet:9,angle:187.5760040283203,house:7};
+
+//longitude[10] = {planet: 10, angle: 244.166, house: 1};
+//longitude[11] = {planet: 11, angle: 17.166, house: 5};
+//longitude[12] = {planet: 12, angle: 16.66, house: 5};
+//longitude[13] = {planet: 13, angle: 239.75, house: 1};
+//longitude[14] = {planet: 14, angle: 109.85, house: 8};
+
